1beb27ce5e935e811fe1b9c958bc88493add2c23
Austin Bubak <abubak2@huskers.unl.edu>
2021-08-26 22:20:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Austin Bubak
+ *
+ * 2021/8/26
+ *
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Austin Bubak\n");
+
+  return 0;
+}
